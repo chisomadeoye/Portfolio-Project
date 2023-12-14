74ee8391a1dec830608d90b3087cb1a7da1d26ec
chisomadeoye <136399135+chisomadeoye@users.noreply.github.com>
2023-12-13 20:28:18 -0500

    COVID-19 Data Exploration Project

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,28 +6,32 @@ order by 3,4
 select *
 From SQLPortfolio..CovidVaccinations
 order by 3,4
-
---- Select the Dara we're going to be using
+----------------------------------------------------------------------
+--- Select the Data we're going to be using
 
 select Location, date, total_cases, new_cases, total_deaths, population
 From SQLPortfolio..CovidDeaths
 where continent is not null
 order by 1,2
 
+ -------------------------------------------------------------------------
 ---Looking at Total Cases vs Total Deaths
+ --Shows liklihood of dying if you contact covid 
 
 select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as MortalityRate
 From SQLPortfolio..CovidDeaths
 where continent is not null
 order by 1,2
 
-----Mortality Rate of Covid in Canada
+-------------------------------------------------------------------------------------
+ ----Showing the liklihood of dying if you got COVID in Canada
 
 select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as MortalityRate
 From SQLPortfolio..CovidDeaths
 where location = 'Canada'
 order by 1,2
-
+ 
+-----------------------------------------------------------------------------------------
 ----Looking at Total Cases vs Population
 ----Shows what percentage of population got COVID
 
@@ -35,8 +39,8 @@ select Location, date, total_cases, population, (total_cases/population)*100 as 
 From SQLPortfolio..CovidDeaths
 order by 1,2
 
-
----- Looking at countries with highest infection rates compared to Population
+-------------------------------------------------------------------------------------
+---- Looking at countries with the highest infection rates compared to the Population
 
 select Location, MAX(total_cases) as HighetInfectionCount, Population, MAX((total_cases/population))*100 as InfectionPercentage
 From SQLPortfolio..CovidDeaths
@@ -44,6 +48,7 @@ where continent is not null
 GROUP BY location, population
 order by InfectionPercentage desc
 
+ -------------------------------------------------------------------------------------
 ----Looking at countries with the highest death count per population
 
 select Location, MAX(cast(total_deaths as int)) as TotalDeaths 
@@ -52,8 +57,8 @@ where continent is not null
 GROUP BY location
 order by TotalDeaths desc
 
-
-------Looking at continet with the highest death counts
+-------------------------------------------------------------------------------------
+------Looking at the continents with the highest death counts
 
 select continent, MAX(cast(total_deaths as int)) as TotalDeaths 
 From SQLPortfolio..CovidDeaths
@@ -62,16 +67,7 @@ GROUP BY continent
 order by TotalDeaths desc
 
 
-------Loooking at continents with highest infection rates compared to population
-
-select continent, MAX(cast(total_cases as int)) as HighetInfectionCount, MAX((total_cases/population))*100 as InfectionPercentage
-From SQLPortfolio..CovidDeaths
-where continent is not null
-GROUP BY continent
-order by InfectionPercentage desc
-
-
-
+-------------------------------------------------------------------------------------
 -----Looking at continents with the highest infection rates compared to the population
 select Location, MAX(total_cases) as HighetInfectionCount, Population, MAX((total_cases/population))*100 as InfectionPercentage
 From SQLPortfolio..CovidDeaths
@@ -81,7 +77,7 @@ order by InfectionPercentage desc
 
 
 
-
+-------------------------------------------------------------------------------------
 ----Looking at Global Numbers by date
 
 select date, sum(new_cases) as TotalCases, sum(cast(new_deaths as int)) as TotalDeaths,  Sum(cast(new_deaths as int))/sum(new_cases) *100 as DeathPercentage
@@ -90,19 +86,21 @@ where continent is not null
 group by date
 order by 1,2
 
-
+-------------------------------------------------------------------------------------
 ---------Looking at total Global Numbers
 select sum(new_cases) as TotalCases, sum(cast(new_deaths as int)) as TotalDeaths,  Sum(cast(new_deaths as int))/sum(new_cases) *100 as DeathPercentage
 From SQLPortfolio..CovidDeaths
 where continent is not null 
 order by 1,2
 
-
-
+-------------------------------------------------------------------------------------
+--------------Working with CovidVacciantions Data
+ 
 Select *
 From SQLPortfolio..CovidVaccinations
 
-----
+-------------------------------------------------------------------------------------
+--------------- Joining CovidDeaths and Covidvaccinations Data
 
 Select *
 From SQLPortfolio..CovidDeaths dea
@@ -111,7 +109,7 @@ on dea.location =  vac.location
 and dea.date = vac.date
 
 
-
+-------------------------------------------------------------------------------------
 ------Looking at how many people on the world have been vaccinated
 
 
@@ -124,7 +122,9 @@ where dea.continent is not null
 order by 1,3
 
 
--------Looking at Total of Vaccinated vs Population using a CTE
+ -------------------------------------------------------------------------------------
+------------Total Population vs Vaccinations
+---------Shows Percentage of Population that recieved at least one dose of vaccine
 
 With PopvsVac (Continent, Location, Date, population, new_vaccinations, NumberOfVaccinations)
 as
@@ -141,14 +141,16 @@ where dea.continent is not null
 select *, (NumberOfVaccinations/population)
 from PopvsVac
 
+-------------------------------------------------------------------------------------
 
-
-
-
-----Creating View to Stoe data for later visuilization
+----Creating View to Store data for later visualization
  
- Create view TotalDeaths as
-select continent, MAX(cast(total_deaths as int)) as TotalDeaths 
-From SQLPortfolio..CovidDeaths
-where continent is not null
-GROUP BY continent
+Create View PercentPopulationVaccinated as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From SQLPortfolio..CovidDeaths dea
+Join SQLPortfolio..CovidVaccinations vac
+on dea.location =  vac.location
+and dea.date = vac.date
+where dea.continent is not null
